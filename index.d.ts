d635c891067e44a6c0f88f2b20f8df83f63a8896
Rahul Bareja <rbareja72@gmail.com>
2020-07-31 14:53:47 +0530

    added declarations

---
+++ index.d.ts
@@ -0,0 +1,51 @@
+import * as React from "react";
+import {
+  StyleProp,
+  TextInputProps,
+  TextStyle,
+  ViewStyle
+} from "react-native";
+
+export interface OTPFieldProps {
+
+  /**
+   * Length of the OTP to be captured.
+   */
+  length?: number,
+
+  /**
+   * Value to displayed in the field. eg. '123'
+   */
+  value?: string,
+
+  /**
+   * Callback when user type in the otp field
+   */
+  onChange?: (value: string) => void,
+
+  /**
+   * Text field style that will be applied to individual text fields
+   */
+  textFieldStyle?: StyleProp<TextInputProps>,
+
+  /**
+   * Style of text fields container
+   */
+  containerStyle?: StyleProp<ViewStyle>,
+
+  /**
+   * Error text.
+   */
+  error?: string,
+
+  /**
+   * Text style for error text by default its red in color.
+   */
+  errorStyle?: StyleProp<TextStyle>
+}
+
+/**
+ * A UI component to take OTP input.
+ */
+
+declare class OTPField extends React.Component<OTPFieldProps, any> { }
